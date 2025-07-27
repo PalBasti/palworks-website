bd71f35877af55f786c895c55d3052b44bd9e41d
PalBasti <aurich@palworks.de>
2025-07-27 16:52:45 +0200

    Update ContractForm.js

--- components/ContractForm.js
+++ components/ContractForm.js
@@ -1,6 +1,17 @@
 import { useState } from 'react'
 import { HelpCircle } from 'lucide-react'
 
+// Tooltip Komponente AUSSERHALB definiert
+const Tooltip = ({ children, text }) => (
+  <div className="group relative inline-block">
+    {children}
+    <div className="invisible group-hover:visible absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 text-sm text-white bg-gray-900 rounded-lg shadow-lg z-10 w-64 text-center">
+      {text}
+      <div className="absolute top-full left-1/2 transform -translate-x-1/2 border-4 border-transparent border-t-gray-900"></div>
+    </div>
+  </div>
+)
+
 export default function ContractForm({ onSubmit }) {
   const [formData, setFormData] = useState({
     // Mietobjekt (VERPFLICHTEND)
@@ -176,16 +187,6 @@ export default function ContractForm({ onSubmit }) {
       default: return 'Mietobjekt an gleicher Adresse wie Vermieter'
     }
   }
-
-  const Tooltip = ({ children, text }) => (
-    <div className="group relative inline-block">
-      {children}
-      <div className="invisible group-hover:visible absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 text-sm text-white bg-gray-900 rounded-lg shadow-lg z-10 w-64 text-center">
-        {text}
-        <div className="absolute top-full left-1/2 transform -translate-x-1/2 border-4 border-transparent border-t-gray-900"></div>
-      </div>
-    </div>
-  )
 
   return (
     <form onSubmit={handleSubmit} className="p-6">
